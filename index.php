555ef11846159dc0d429cf7d602d42a76bbcf580
Utkarsh Goel <66838142+utkarshgoel10@users.noreply.github.com>
2021-10-14 23:18:26 +0530

    Update index.php

--- index.php
+++ index.php
@@ -20,7 +20,7 @@
 <nav class="navbar navbar-expand-lg">
    <div class="container" style="position: relative; top: 5px">
       <a class="navbar-brand" href="index.php">
-      <img src="logo.png" height="30.17px" width="auto" style="position: relative; top: -2px;" alt="">
+      <img src="media/logo.png" height="30.17px" width="auto" style="position: relative; top: -2px;" alt="">
       </a>
       <button class="navbar-toggler navbar-light" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
@@ -54,10 +54,10 @@
             </li>
          </ul>
          <a class="navbar-brand" href="#">
-         <img src="gplay.png" height="40px" width="auto">
+         <img src="media/gplay.png" height="40px" width="auto">
          </a>
          <a class="navbar-brand" href="#">
-         <img src="applestore.png" height="40px" width="auto">
+         <img src="media/applestore.png" height="40px" width="auto">
          </a>
       </div>
    </div>
@@ -74,7 +74,7 @@
          <a href="upload.php" style="text-decoration: none; color: inherit;"><button class="btn bttn bttn3">Publish</button></a>
       </div>
       <div class="col-sm-6">
-         <img id="head-img" src="head.png" width="570px" height="auto" style="position:relative;top: -55px;">
+         <img id="head-img" src="media/head.png" width="570px" height="auto" style="position:relative;top: -55px;">
       </div>
    </div>
 </div>
@@ -132,7 +132,7 @@
             <h5 class="card-h5">Mobile <br>Responsive</h5>
          </div>
          <div>
-            <img src="icon1.png" class="icon-h5">
+            <img src="media/icon1.png" class="icon-h5">
          </div>
       </div>
       <div class="cards-div col-12 col-sm-3 col-lg-3 col-md-3 col-xl-3" style="left: 285px;">
@@ -140,7 +140,7 @@
             <h5 class="card-h5">User <br>Friendly</h5>
          </div>
          <div>
-            <img src="icon2.png" class="icon-h5">
+            <img src="media/icon2.png" class="icon-h5">
          </div>
       </div>
       <div class="cards-div col-12 col-sm-3 col-lg-3 col-md-3 col-xl-3" style="left: 570px;">
@@ -148,7 +148,7 @@
             <h5 class="card-h5">Well Organised Layers</h5>
          </div>
          <div>
-            <img src="icon3.png" class="icon-h5">
+            <img src="media/icon3.png" class="icon-h5">
          </div>
       </div>
       <div class="cards-div col-12 col-sm-3 col-lg-3 col-md-3 col-xl-3" style="left: 855px;">
@@ -156,7 +156,7 @@
             <h5 class="card-h5">Easily Customisable</h5>
          </div>
          <div>
-            <img src="icon4.png" class="icon-h5">
+            <img src="media/icon4.png" class="icon-h5">
          </div>
       </div>
    </div>
@@ -166,7 +166,7 @@
             <h5 class="card-h5">Better Components</h5>
          </div>
          <div>
-            <img src="icon5.png" class="icon-h5">
+            <img src="media/icon5.png" class="icon-h5">
          </div>
       </div>
       <div class="cards-div col-12 col-sm-3 col-lg-3 col-md-3 col-xl-3" style="left: 285px; top: 265px;">
@@ -174,7 +174,7 @@
             <h5 class="card-h5">Multiple <br>Blocks</h5>
          </div>
          <div>
-            <img src="icon6.png" class="icon-h5">
+            <img src="media/icon6.png" class="icon-h5">
          </div>
       </div>
       <div class="cards-div col-12 col-sm-3 col-lg-3 col-md-3 col-xl-3" style="left: 570px; top: 265px;">
@@ -182,7 +182,7 @@
             <h5 class="card-h5">Robust <br>Workflow</h5>
          </div>
          <div>
-            <img src="icon7.png" class="icon-h5">
+            <img src="media/icon7.png" class="icon-h5">
          </div>
       </div>
       <div class="cards-div col-12 col-sm-3 col-lg-3 col-md-3 col-xl-3" style="left: 855px; top: 265px;">
@@ -190,12 +190,12 @@
             <h5 class="card-h5">Mobile <br>Responsive</h5>
          </div>
          <div>
-            <img src="icon8.png" class="icon-h5">
+            <img src="media/icon8.png" class="icon-h5">
          </div>
       </div>
    </div>
    <div class="circ-div">
-      <img class="circ" src="circle.png">
+      <img class="circ" src="media/circle.png">
    </div>
 </div>
 
@@ -203,7 +203,7 @@
 
 <div class="container" style="margin-top: 225px">
    <div style="position: relative; ">
-      <img src="bottom.png" style="width: 100%; display: block;margin-left: auto;margin-right: auto;height: auto;">
+      <img src="media/bottom.png" style="width: 100%; display: block;margin-left: auto;margin-right: auto;height: auto;">
       <h3 class="bottom-txt" style="position: absolute; left: 480px; top: 18px;">Join 569 more people in the waitlist</h3>
       <div class="search-white" style="position: absolute; left: 480px; top: 188px;">
          <p id="search-white-txt">Your work email address</p>
@@ -220,7 +220,7 @@
    <div class="container">
       <div class="col-lg-8" style="float: left;">
          <div class="foot-logo" style="margin-top: 50px;">
-            <img src="company-logo.png" height="30px" width="auto">
+            <img src="media/company-logo.png" height="30px" width="auto">
          </div>
          <div class="foot-line1" style="margin-top: 30px;">
             <a href="#" style="order: 0;">Download Now</a>
@@ -244,12 +244,12 @@
          </div>
          <div style="padding-top: 12px;">
             <a href="#">
-            <img src="applestore.png" height="40px" width="auto">
+            <img src="media/applestore.png" height="40px" width="auto">
             </a>
          </div>
          <div style="padding-top: 12px;">
             <a href="#">
-            <img src="gplay.png" height="40px" width="auto">
+            <img src="media/gplay.png" height="40px" width="auto">
             </a>
          </div>
       </div>
